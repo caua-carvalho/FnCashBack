7b10afa9167cb239e72498c182c12fe3011bc70c
Caua Carvalho <caua.carvalho@outlook.com.br>
2026-01-26 20:18:49 -0300

    index origianl

--- src/public/index.php
+++ src/public/index.php
@@ -1,19 +1,52 @@
 <?php
 
+declare(strict_types=1);
+
+// Erros
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "PHP OK<br>";
+// CORS
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Max-Age: 86400');
 
-$root = dirname(__DIR__);
-echo "ROOT = $root<br>";
-
-if (!file_exists($root . '/Router.php')) {
-    echo "Router.php NÃO EXISTE<br>";
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
     exit;
 }
 
-require_once $root . '/Router.php';
+header('Content-Type: application/json');
 
-echo "Router carregado";
+// Raiz do projeto
+$root = dirname(__DIR__);
+
+// Core
+require_once $root . '/Router.php';
+require_once $root . '/middleware_helpers.php';
+
+// Controllers
+require_once $root . '/Controllers/HomeController.php';
+require_once $root . '/Controllers/TransactionController.php';
+require_once $root . '/Controllers/CreateTokenController.php';
+
+// Router
+$router = new Router();
+
+// Rotas públicas
+$router->get('/', [HomeController::class, 'index']);
+$router->get('/about', [HomeController::class, 'about']);
+$router->get('/createToken', [CreateToken::class, 'index']);
+
+// Rotas protegidas
+$router->get('/transactions', withAuth([TransactionController::class, 'index']));
+$router->get('/transactions/show', withAuth([TransactionController::class, 'show']));
+$router->post('/transactions', withAuth([TransactionController::class, 'create']));
+$router->post('/transactions/update', withAuth([TransactionController::class, 'update']));
+$router->post('/transactions/delete', withAuth([TransactionController::class, 'destroy']));
+$router->post('/transactions/audio', withAuth([TransactionController::class, 'storeAudio']));
+
+// Dispatch
+$router->dispatch();

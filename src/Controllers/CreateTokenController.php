a0e8a8b31b6a387b1be42a3bb186f41f7d0e4bc6
Caua Carvalho <caua.carvalho@outlook.com.br>
2026-01-26 21:31:37 -0300

    ajuste no env create token

--- src/Controllers/CreateTokenController.php
+++ src/Controllers/CreateTokenController.php
@@ -7,7 +7,7 @@ class CreateToken
 {
     public function index(): void
     {
-        $token = generateJWT(['id' => '21603a56-2bf2-4a69-a109-a8ef3baac986', 'role' => 'admin'], 'changeme');
+        $token = generateJWT(['id' => '21603a56-2bf2-4a69-a109-a8ef3baac986', 'role' => 'admin'], $_ENV['JWT_SECRET']);
         echo $token;
     }
 }

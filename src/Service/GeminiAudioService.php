e4223c00915d9642ef0746cd1f6956c37c1501e4
Caua Carvalho <caua.carvalho@outlook.com.br>
2026-01-27 10:09:42 -0300

    depuracao da api do gemini

--- src/Service/GeminiAudioService.php
+++ src/Service/GeminiAudioService.php
@@ -63,6 +63,8 @@ class GeminiAudioService
         ]);
 
         $response = curl_exec($ch);
+
+        error_log("[GeminiAudioService] response bruto:" . $response)
         if ($response === false) {
             throw new \RuntimeException("Erro curl (start): " . curl_error($ch));
         }
